2567f1bfab2c7a20058b1712148666644fcf57da
Rick Hurst <rick.hurst@automattic.com>
2025-01-23 10:49:50 +0000

    formatting and renaming towards meeting wp coding standards

--- vip-learn-audit.php
+++ vip-learn-audit.php
@@ -12,15 +12,15 @@
 
 // Prevent direct access to the file.
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 // Include library classes
-require_once plugin_dir_path( __FILE__ ) . 'inc/class.punctuation.php';
-require_once plugin_dir_path( __FILE__ ) . 'inc/class.title-case.php';
-require_once plugin_dir_path( __FILE__ ) . 'inc/class.sentence-case.php';
-require_once plugin_dir_path( __FILE__ ) . 'inc/class.time-estimation.php';
+require_once plugin_dir_path( __FILE__ ) . 'inc/class-punctuation.php';
+require_once plugin_dir_path( __FILE__ ) . 'inc/class-title-case.php';
+require_once plugin_dir_path( __FILE__ ) . 'inc/class-sentence-case.php';
+require_once plugin_dir_path( __FILE__ ) . 'inc/class-time-estimation.php';
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
-    require_once plugin_dir_path( __FILE__ ) . 'inc/class.wp-cli-commands.php';
+	require_once plugin_dir_path( __FILE__ ) . 'inc/class-wp-cli-commands.php';
 }
